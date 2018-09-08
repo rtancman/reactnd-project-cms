9b8869e62269df0b78c1936a15f5a2d23030f191
Raffael Tancman <rtancman@gmail.com>
2018-09-07 21:03:23 -0300

    feat: Add Redux in project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import './index.css';
 import App from './components/App';
 
+const store = configureStore();
+
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
-  document.getElementById('root')
-)
+    <Provider store={store}>
+        <BrowserRouter><App /></BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+);
