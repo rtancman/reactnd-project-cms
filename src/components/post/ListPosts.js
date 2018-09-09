80dfe3b9eefd3a1e4a8d5bed3a18201296ee9ae4
Raffael Tancman <raffael.tancman@resultadosdigitais.com.br>
2018-09-09 18:23:30 -0300

    feat: Connect ListPosts in store

--- src/components/post/ListPosts.js
+++ src/components/post/ListPosts.js
@@ -1,16 +1,67 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { postsFetchData } from './actions';
 
 class ListPosts extends Component {
+  static propTypes = {
+    fetchData: PropTypes.func.isRequired,
+    items: PropTypes.array.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    didInvalidate: PropTypes.bool.isRequired
+  }
+
+  componentDidMount() {
+    this.props.fetchData()
+  }
+
   render() {
+    let content = ''
+    const { items, isFetching, didInvalidate } = this.props
+
+    if (didInvalidate) {
+      content = (<p>Sorry! There was an error loading the items</p>)
+    } else if (isFetching) {
+      content = (
+        <div>
+          <CircularProgress thickness={7} />
+          <p>Loading…</p>
+        </div>
+      )
+    } else if ( items.length > 0 ) {
+      content = (
+        <ul>
+          {items.map((item) => (
+            <li key={item.id}>
+              {item.title}
+            </li>
+          ))}
+        </ul>
+      )
+    }
+
     return (
       <div className="ListPosts">
         <h2>Posts</h2>
-        <ul>
-          <li></li>
-        </ul>
+        { content }
       </div>
     );
   }
 }
 
-export default ListPosts;
+const mapStateToProps = ({ posts }) => {
+  return {
+    items: posts.items,
+    didInvalidate: posts.didInvalidate,
+    isFetching: posts.isFetching
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchData: () => dispatch(postsFetchData())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListPosts);
