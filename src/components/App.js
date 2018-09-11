6f3d4acefb1e565eb249e29b16993d532d9e6390
Raffael Tancman <rtancman@gmail.com>
2018-09-11 12:43:52 -0300

    feat: Create component Category
    - Routes pass props to pages

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,21 @@ import { Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home'
-import Category from './pages/Category'
+import CategoryPage from './pages/CategoryPage'
 import Post from './pages/Post'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path='/' render={({ history }) => (
-          <Home />
+        <Route exact path='/' render={(props) => (
+          <Home {...props} />
         )}/>
-        <Route path='/category/:id' render={({ history }) => (
-          <Category />
+        <Route path='/category/:id' render={(props) => (
+          <CategoryPage {...props} />
         )}/>
-        <Route path='/post/:id' render={({ history }) => (
-          <Post />
+        <Route path='/post/:id' render={(props) => (
+          <Post {...props} />
         )}/>
       </div>
     );
--- src/components/pages/CategoryPage.js
+++ src/components/pages/CategoryPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-
+import Category from 'components/category/Category'
 
 const styles = theme => ({
   root: {
@@ -10,20 +10,23 @@ const styles = theme => ({
   }
 });
 
-const Category = (props) => {
-  const { classes } = props;
+const CategoryPage = (props) => {
+  const { classes, match } = props;
 
   return (
     <div className={classes.root}>
       <h1>Category</h1>
       <Grid container spacing={24}>
+        <Grid item xs={12}>
+          <Category categoryId={match.params.id} />
+        </Grid>
       </Grid>
     </div>
   )
 }
 
-Category.propTypes = {
+CategoryPage.propTypes = {
   classes: PropTypes.object.isRequired,
 }
 
-export default withStyles(styles)(Category);
+export default withStyles(styles)(CategoryPage);
