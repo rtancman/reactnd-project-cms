ff7d09bfdf55932a1c88ecef7459c3b800871503
Raffael Tancman <rtancman@gmail.com>
2018-09-15 18:10:23 -0300

    fix: Add switch to select one route only

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router'
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home'
 import CategoryPage from './pages/CategoryPage'
 import PostPage from './pages/PostPage'
+import PostAdminPage from './pages/PostAdminPage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path='/' render={(props) => (
-          <Home {...props} />
-        )}/>
-        <Route path='/category/:id' render={(props) => (
-          <CategoryPage {...props} />
-        )}/>
-        <Route path='/posts/:id' render={(props) => (
-          <PostPage {...props} />
-        )}/>
+        <Switch>
+          <Route exact path='/' render={(props) => (
+            <Home {...props} />
+          )}/>
+          <Route exact path='/category/:id' render={(props) => (
+            <CategoryPage {...props} />
+          )}/>
+          <Route exact path='/posts/create' render={(props) => (
+            <PostAdminPage {...props} />
+          )}/>
+          <Route exact path='/posts/:id' render={(props) => (
+            <PostPage {...props} />
+          )}/>
+        </Switch>
       </div>
     );
   }
